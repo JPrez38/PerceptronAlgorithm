7f7da1a9f07c977af4c347247c9d50b7dc60bcf3
Jacob Preston <jacobpreston4@gmail.com>
2013-09-13 14:05:14 -0400

    added feature vector

--- Main.scala
+++ Main.scala
@@ -1,4 +1,5 @@
 import math._
+import scala.collection.mutable.Map
 import scala.collection.mutable.HashMap
 
 def main(args: Array[String]) {
@@ -11,8 +12,19 @@ def main(args: Array[String]) {
 	val tmp = emailList.splitAt(trainingSize)
 	val trainingDataSet = tmp._1
 	val validationDataSet = tmp._2
-	val vocab = buildVocabulary(trainingDataSet)
-	//println(vocab.size + "," + emailList.size)
+	//trainingDataSet.foreach(email => println(email))
+	val vocabTally = buildVocabulary(trainingDataSet)
+	var vocabList = new Array[String](vocabTally.size)
+	var vocabIndex = 0
+	for(word <- vocabTally) { //converts to simple array for faster iteration
+		vocabList(vocabIndex) = word._1
+		vocabIndex+=1
+	}
+	//vocabTally.foreach(word => println(word))
+	val featureVectors = makeFeatureVector(validationDataSet,vocabList)
+	//println(featureVectors.get(validationDataSet(5)))
+	//featureVectors.foreach(vect => println(vect))
+	//println(vocabList.size + "," + emailList.size)
 	val end = System.currentTimeMillis
 
 	println("Running time: " + (end-start) + " millis")
@@ -44,14 +56,12 @@ def getEmailList(inputFile: String) : List[(Char,String)] = {
 	return emails.reverse
 }
 
-def buildVocabulary(emailList: List[(Char,String)]) : HashMap[String,Int] = {
-	var vocabulary = new HashMap[String,Int]()
+def buildVocabulary(emailList: List[(Char,String)]) : Map[String,Int] = {
+	var vocabulary = Map[String,Int]()
 	for (email <- emailList) {
 		val words = email._2.split(" ")
 		var wordList = Set[String]()
-		for (word <- words) {
-			wordList = wordList + word
-		}
+		words.foreach(word => wordList += word)
 		for (word <- wordList) {
 			var count = vocabulary.getOrElse(word,0)
 			count += 1
@@ -59,8 +69,29 @@ def buildVocabulary(emailList: List[(Char,String)]) : HashMap[String,Int] = {
 		}
 	}
 	vocabulary.remove("")
-	return vocabulary.retain((k,v) => v > 30)
+	return vocabulary.retain((k,v) => v >= 30)
 } 
+
+
+
+def makeFeatureVector(emailList: List[(Char,String)], vocabList: Array[String]) : Map[(Char,String),List[Int]] = {
+	var emailFeatureVector = Map[(Char,String),List[Int]]()
+	for (email <- emailList) {
+		var featureVector = List[Int](vocabList.size)
+		var vocabIndex = 0
+		val words = email._2.split(" ")
+		var wordList = HashMap[String,Int]()
+		words.foreach(word => wordList += word -> 0)
+		for (vocabWord <- vocabList) {
+			if(wordList.getOrElse(vocabWord,None) != None) featureVector ::= 1 else featureVector ::= 0
+			vocabIndex+=1
+		}
+		//featureVector.foreach(vect => println(vect))
+		emailFeatureVector += email -> featureVector
+		//emailFeatureVector.get(email).foreach(x => println(x))
+	}
+	return emailFeatureVector
+}
 
 val args = Array[String]()
 main(args)
